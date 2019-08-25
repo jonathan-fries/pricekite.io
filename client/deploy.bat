9981d81ded48e5086e39be30275986691d6e5858
jfries <jonathan.fries@gmail.com>
2019-08-25 14:14:23 -0600

    Responsive menu functions, but is ugly.

--- client/deploy.bat
+++ client/deploy.bat
@@ -1,3 +1,3 @@
 
 
-gsutil rsync -R c:/Users/jfries/Documents/GitHub/pricekite.io/client/public gs://www.pricekite.io & gsutil rsync -R c:/Users/jfries/Documents/GitHub/pricekite.io/client/public gs://www.pricekite.io
+gsutil rsync -R c:/Users/jfries/Documents/GitHub/pricekite.io/client/dist gs://www.pricekite.io & gsutil rsync -R c:/Users/jfries/Documents/GitHub/pricekite.io/client/public gs://www.pricekite.io
