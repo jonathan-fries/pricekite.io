73d06641a738d0ddac3380bb392b3116163c7b27
jfries <jonathan.fries@gmail.com>
2019-08-25 08:24:44 -0600

    Addition of footer and core styles.

---
+++ client/src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './footer.scss';
+
+export default class Header extends React.Component {
+
+    render() {
+        return <div id="footer">
+                  <div id="footerText">© 2019 by Jonathan Fries</div>
+              </div>
+    }
+
+}
