76a9ed3667eb8b16666fba5f01dfec201ab52dfe
Jonathan <jonathan.fries@gmail.com>
2019-08-30 10:40:32 -0600

    Updated for allowing CORS access.

--- client/src/components/price_display/static_ip_address/google_ip_address.js
+++ client/src/components/price_display/static_ip_address/google_ip_address.js
@@ -2,12 +2,38 @@ import React from 'react';
 
 export default class GoogleIpAddress extends React.Component{
 
+  constructor(props){
+        super(props);
+        this.state = {gcpPriceItem:{ monthly: '', skuId: ''}};
+
+        var ws = "https://us-central1-pricekite.cloudfunctions.net/ip-addr-prices";
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', ws);
+        xhr.onload = () => {
+            if(xhr.status === 200){
+                console.log(xhr.responseText);
+                var local_gcpPriceItems = [];
+                local_gcpPriceItem = JSON.parse(xhr.responseText);
+                this.setState({gcpPriceItem:local_gcpPriceItem});
+            }
+            else{
+                console.log("Error calling web service.")
+            }
+        };
+        xhr.send();
+
+    }
+
   render(){
+
+      const gcpPriceItems = this.props.gcpPriceItems;
+
       return <table width="100%">
                 <tbody>
-                    <tr><td width="30%">In Use (Standard VM):</td><td>$2.88/month</td></tr>
-                    <tr><td width="30%">In Use (Preemptible VM):</td><td>$1.44/month</td></tr>
-                    <tr><td>Not in Use:</td><td>$7.20/month</td></tr>
+                    <tr><td>Not in Use:</td><td>{gcpPriceItem.monthly}</td><td>{gcpPriceItem.skuId}</td></tr>
+                    <tr><td width="30%">In Use (Standard VM):</td><td>$2.88/month</td><td>For future, no SKU</td></tr>
+                    <tr><td width="30%">In Use (Preemptible VM):</td><td>$1.44/month</td><td>For future, no SKU</td></tr>
                 </tbody></table>;
           }
 
