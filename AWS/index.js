8b4f55f631e79f4cfe17fd7686c238917cc83091
Jonathan <jonathan.fries@gmail.com>
2019-09-01 15:18:58 -0600

    Nearly working AWS.

--- AWS/index.js
+++ AWS/index.js
@@ -11,9 +11,9 @@ exports.ipAddressPrice = async (event) => {
             Value: "AmazonEC2"
            },
          {
-        Field: "productFamily",
+        Field: "sku",
         Type: "TERM_MATCH",
-        Value: "IP Address"
+        Value: "NA5BT4DMBGMMK5MT"
        }
       ],
       FormatVersion: "aws_v1",
@@ -38,9 +38,36 @@ exports.ipAddressPrice = async (event) => {
             }
             else
             {
-                console.log("We got something, let us print it.");
-                console.log(data);
-                resolve(data);
+
+
+                console.log("We got something.");
+                var raw = data;
+                var pricingJson = {};
+                var onDemand;
+                var priceDimensions;
+
+                pricingJson.name = "Unused IP";
+                pricingJson.sku = raw.PriceList[0].product.sku;
+
+                //now it is time to parse some JSON
+                //this BS is required because AWS puts periods in their flippin' JSON object names!
+                onDemand = raw.PriceList[0].terms.OnDemand;
+                priceDimensions = Object.values(onDemand)[0].priceDimensions;
+                pricingJson.hourly = Object.values(priceDimensions)[0].pricePerUnit.USD;
+
+                //pricingJson.hourly = raw.PriceList[0].terms.OnDemand.a.priceDimensions.a.pricePerUnit.USD;
+                pricingJson.monthly = pricingJson.pricePerUnit * 24 * 30;
+
+                var response = {
+                    "statusCode": 200,
+                    "headers": {
+                        "my_header": "my_value"
+                    },
+                    "body": JSON.stringify(pricingJson),
+                    "isBase64Encoded": false
+                };
+
+                resolve(response);
             }// successful response
         });
     });
