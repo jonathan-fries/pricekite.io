78399e895b1fdf2323342f5b92410d63bd42a709
Jonathan <jonathan.fries@gmail.com>
2019-09-03 16:33:59 -0600

    Azure now displays returned values.

--- client/src/components/price_display/static_ip_address/azure_ip_address.js
+++ client/src/components/price_display/static_ip_address/azure_ip_address.js
@@ -4,7 +4,45 @@ import './column_styles.scss'
 
 export default class AzureIpAddress extends React.Component{
 
+  constructor(props){
+        super(props);
+        this.state = {azurePriceItems:[{ name: 'Thinking...', monthly: '', skuId: ''}]};
+
+        var ws = "https://pricekite-io.azurewebsites.net/api/ipAddrPrice";
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', ws);
+
+        xhr.onload = () => {
+            if(xhr.status === 200){
+                console.log(xhr.responseText);
+                var local_azurePriceItem = [];
+                local_azurePriceItem = JSON.parse(xhr.response);
+                local_azurePriceItem = JSON.parse(local_azurePriceItem);
+                this.setState({azurePriceItems:local_azurePriceItem});
+            }
+            else{
+                console.log("Error calling web service.")
+            }
+        };
+        xhr.send();
+
+    }
+
   render(){
+
+    const prices = [];
+    var i = 0;
+
+    for(i; i < this.state.azurePriceItems.length; i++)
+    {
+      prices.push( <tr key={i}>
+        <td>{this.state.azurePriceItems[i].name}</td>
+        <td>${(+this.state.azurePriceItems[i].monthly).toFixed(2)}</td>
+        <td>{this.state.azurePriceItems[i].sku}</td>
+      </tr> );
+    }
+
     return <Table striped bordered hover responsive="sm">
       <thead>
         <tr>
@@ -14,31 +52,7 @@ export default class AzureIpAddress extends React.Component{
         </tr>
       </thead>
       <tbody>
-      <tr>
-        <td>ASM First 5:</td>
-        <td>$0/month</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>ASM Additional:</td>
-        <td>$2.59/month</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>Basic ARM First 5:</td>
-        <td>$2.88/month</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>Basic ARM Additional:</td>
-        <td>$5.76/month</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>Standard ARM All:</td>
-        <td>$3.60/month</td>
-        <td></td>
-      </tr>
+      {prices}
       </tbody>
     </Table>;
   }
