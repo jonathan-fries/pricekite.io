ef7d7500038a74353a16da5e07a8a5d9fe670aee
Jonathan <jonathan.fries@gmail.com>
2019-09-05 11:26:56 -0600

    Animating loading indicator.

--- client/src/components/status_indicator/status_indicator.js
+++ client/src/components/status_indicator/status_indicator.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
 import './status_indicator.scss'
+import { Wave } from 'react-animated-text';
+
 //import { Wave } from 'react-animated-text';
 
 export default class StatusIndicator extends React.Component{
 
   constructor(props){
         super(props);
+
+        this.state = { gcpState: {gcp_loading: true, gcp_alive: false }, awsState:{aws_loading:true, aws_alive: false}, azure_state:{azure_loading: true, azure_alive: false} };
+
       }
 
   render(){
@@ -20,9 +25,9 @@ export default class StatusIndicator extends React.Component{
       </thead>
       <tbody>
         <tr>
-          <td>Alive</td>
-          <td>Alive</td>
-          <td>Alive</td>
+          <td><Wave text="......" effect="fadeOut"/></td>
+          <td><Wave text="......" effect="fadeOut"/></td>
+          <td><Wave text="......" effect="fadeOut"/></td>
         </tr>
       </tbody>
     </Table>
