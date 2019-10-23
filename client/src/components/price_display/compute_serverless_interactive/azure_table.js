a5ee225fc9089622784ae4ed6498dbf9652cf219
Jonathan <jonathan.fries@gmail.com>
2019-10-23 13:54:43 -0600

    Adding core structure to google, AWS, Azure tables.

---
+++ client/src/components/price_display/compute_serverless_interactive/azure_table.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//import { Wave } from 'react-animated-text';
+
+export default class AzureTable extends React.Component{
+
+  constructor(props){
+        super(props);
+      }
+
+      render(){
+        return <div></div>;
+      }
+
+}
