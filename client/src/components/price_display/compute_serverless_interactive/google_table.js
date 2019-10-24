2d3c026e831ca7a688e25c92a783b22f7c75991e
Jonathan <jonathan.fries@gmail.com>
2019-10-24 12:39:10 -0600

    Google working with formatting, clean blank formatting, and CPU calculations.

--- client/src/components/price_display/compute_serverless_interactive/google_table.js
+++ client/src/components/price_display/compute_serverless_interactive/google_table.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
+//import '../shared/google_memory_cpu_assignments.js';
 
 //import { Wave } from 'react-animated-text';
 
@@ -11,8 +12,10 @@ export default class GoogleTable extends React.Component{
         this.getInvocationRecord = this.getInvocationRecord.bind(this);
         this.getCPURecord = this.getCPURecord.bind(this);
         this.getMemoryRecord = this.getMemoryRecord.bind(this);
-
+        const cpuAssignments = require('../shared/google_memory_cpu_assignments.js');
+        this.state = { gcp_cpu_assignments : cpuAssignments }
       }
+
 
       render(){
         const regionSelected = this.props.regionSelected;
@@ -24,9 +27,10 @@ export default class GoogleTable extends React.Component{
 
         const googleInvocations = this.getInvocationRecord(googleData, numberOfFunctions, functionInvocations, 2000000);
         const googleMemory = this.getMemoryRecord(googleData, numberOfFunctions, functionInvocations, functionAverageTime, functionMemoryAmount, 400000);
-        const googleCpu = this.getCPURecord(googleData);
+        const googleCpu = this.getCPURecord(googleData, numberOfFunctions, functionInvocations, functionAverageTime, functionMemoryAmount, 200000);
 
         return <div >
+        <h2>Google</h2>
         <Table striped bordered hover responsive="sm">
           <thead>
             <tr>
@@ -43,17 +47,25 @@ export default class GoogleTable extends React.Component{
               <td>{googleInvocations.name}</td>
               <td>${googleInvocations.pricePerUnit != 0.00 ? (googleInvocations.pricePerUnit).toFixed(8) : "--"}</td>
               <td>{googleInvocations.unit}</td>
-              <td>{googleInvocations.unitsConsumed.toLocaleString('en')}</td>
-              <td>{googleInvocations.unitsCharged.toLocaleString('en')}</td>
-              <td>${googleInvocations.cost.toFixed(2)}</td>
+              <td>{googleInvocations.name != "N/A" ? googleInvocations.unitsConsumed.toLocaleString('en') : "--"}</td>
+              <td>{googleInvocations.name != "N/A" ? googleInvocations.unitsCharged.toLocaleString('en') : "--"}</td>
+              <td>${googleInvocations.name != "N/A" ? googleInvocations.cost.toFixed(2) : "--"}</td>
             </tr>
             <tr>
               <td>{googleMemory.name}</td>
-              <td>${(googleMemory.pricePerUnit).toFixed(8)}</td>
+              <td>${googleMemory.pricePerUnit != 0.00 ? (googleMemory.pricePerUnit).toFixed(8) : "--"}</td>
               <td>{googleMemory.unit}</td>
-              <td>{googleMemory.unitsConsumed.toLocaleString('en')}</td>
-              <td>{googleMemory.unitsCharged.toLocaleString('en')}</td>
-              <td>${googleMemory.cost.toFixed(2)}</td>
+              <td>{googleMemory.name != "N/A" ? googleMemory.unitsConsumed.toLocaleString('en') : "--"}</td>
+              <td>{googleMemory.name != "N/A" ? googleMemory.unitsCharged.toLocaleString('en') : "--"}</td>
+              <td>${googleMemory.name != "N/A" ? googleMemory.cost.toFixed(2) : "--"}</td>
+            </tr>
+            <tr>
+              <td>{googleCpu.name}</td>
+              <td>${googleCpu.pricePerUnit != 0.00 ? (googleCpu.pricePerUnit).toFixed(8) : "--"}</td>
+              <td>{googleCpu.unit}</td>
+              <td>{googleCpu.name != "N/A" ? googleCpu.unitsConsumed.toLocaleString('en') : "--"}</td>
+              <td>{googleCpu.name != "N/A" ? googleCpu.unitsCharged.toLocaleString('en') : "--"}</td>
+              <td>${googleCpu.name != "N/A" ? googleCpu.cost.toFixed(2) : "--"}</td>
             </tr>
           </tbody>
         </Table>
@@ -62,7 +74,7 @@ export default class GoogleTable extends React.Component{
 
       getInvocationRecord(googleData, numberOfFunctions, functionInvocations, invocationDiscount){
         var i = 0;
-        var invocationRecord = { "name" : "N/A", "pricePerUnit" : 0 };
+        var invocationRecord = { "name" : "N/A", "pricePerUnit" : 0, "unit" : "N/A", "unitsCharged": 0, "unitsConsumed" : 0, "cost" : 0 };
         for(i; i< googleData.length; i++)
         {
           if(googleData[i].name == "Invocations")
@@ -82,9 +94,9 @@ export default class GoogleTable extends React.Component{
         return invocationRecord;
       }
 
-      getCPURecord(googleData){
+      getCPURecord(googleData, numberOfFunctions, functionInvocations, functionAverageTime, functionMemoryAmount, cpuDiscount){
         var i = 0;
-        var cpuRecord;
+        var cpuRecord = { "name" : "N/A", "pricePerUnit" : 0, "unit" : "N/A", "unitsCharged": 0, "unitsConsumed" : 0, "cost" : 0 };
         for(i; i< googleData.length; i++)
         {
           if(googleData[i].name == "CPU Time")
@@ -92,12 +104,23 @@ export default class GoogleTable extends React.Component{
             cpuRecord = googleData[i];
           }
         }
+
+        if(cpuRecord.name != "N/A")
+        {
+          var cpuFraction = (this.state.gcp_cpu_assignments[functionMemoryAmount].cpu)/1000;
+
+          cpuRecord.unitsConsumed = (cpuFraction * ((functionInvocations * functionAverageTime)/1000)) * numberOfFunctions;
+          cpuRecord.unitsCharged = cpuRecord.unitsConsumed - cpuDiscount;
+          if(cpuRecord.unitsCharged < 0){cpuRecord.unitsCharged = 0};
+          cpuRecord.cost = cpuRecord.unitsCharged * cpuRecord.pricePerUnit;
+        }
+
         return cpuRecord;
       }
 
       getMemoryRecord(googleData, numberOfFunctions, functionInvocations, functionAverageTime, functionMemoryAmount, memoryDiscount){
         var i = 0;
-        var memoryRecord = { "name" : "N/A" };
+        var memoryRecord = { "name" : "N/A", "pricePerUnit" : 0, "unit" : "N/A", "unitsCharged": 0, "unitsConsumed" : 0, "cost" : 0 };
         for(i; i< googleData.length; i++)
         {
           if(googleData[i].name == "Memory Time")
