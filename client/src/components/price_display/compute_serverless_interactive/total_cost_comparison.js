7006886935eb078e121f27346b6aecd24098be8b
Jonathan <jonathan.fries@gmail.com>
2019-10-24 13:22:32 -0600

    Working on total cost table.

---
+++ client/src/components/price_display/compute_serverless_interactive/total_cost_comparison.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class TotalCostComparison extends React.Component{
+
+  constructor(props){
+        super(props);
+      }
+
+      render(){
+        return <div></div>;
+      }
+
+}
